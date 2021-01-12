1934ecfcdeab2d6b957c5e029da6c1b288caaf30
Pedro <a2019114887@estudiantes.edu.bo>
2021-01-12 19:34:23 -0400

    mi primer commit

---
+++ Primer_consola/Primer_consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primer_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
